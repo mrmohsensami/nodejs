0993832c0b3edaf8f82802ba0ef55d30fc3483da
mohsensami <mohsensami18727@gmail.com>
2024-09-26 17:25:06 +0330

    working with ejs template engine

--- app.js
+++ app.js
@@ -11,25 +11,30 @@ app.set('views', 'templates');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        { title: 'title one', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        { title: 'title two', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        { title: 'title three', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+    ];
+    res.render('index', { title: 'Home', blogs });
     // res.sendFile('./views/index.html', {root: __dirname})
 });
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'about' });
     // res.sendFile('./views/about.html', {root: __dirname})
 });
 app.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'create' });
     // res.sendFile('./views/about.html', {root: __dirname})
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+    // res.sendFile('./views/404.html', {root: __dirname})
 });
 
 // redirect
 // app.get('/about-us', (req, res)=>{
 //     res.redirect('/about')
 // })
-
-// 404 page
-app.use((req, res) => {
-    res.status(404).render('404');
-    // res.sendFile('./views/404.html', {root: __dirname})
-});
